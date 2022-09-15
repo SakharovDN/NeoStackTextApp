d01ee8714aa1ed5a15ec43b133c54de574a13745
SakharovDN <deafsah@gmail.com>
2022-09-15 22:29:20 +0700

    Added function class

---
+++ NeoStackTextApp/Models/Coordinates.cs
@@ -0,0 +1,22 @@
+﻿namespace NeoStackTextApp.Models;
+
+public class Coordinates
+{
+    #region Properties
+
+    public double X { get; set; }
+
+    public double Y { get; set; }
+
+    #endregion
+
+    #region Constructors
+
+    public Coordinates(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    #endregion
+}
