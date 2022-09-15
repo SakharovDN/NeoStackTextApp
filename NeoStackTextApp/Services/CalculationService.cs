a3f52a88078b1b8ccc16d2f3770b2b903e3f386d
SakharovDN <deafsah@gmail.com>
2022-09-15 23:39:21 +0700

    Added calculation service

---
+++ NeoStackTextApp/Services/CalculationService.cs
@@ -0,0 +1,34 @@
+﻿namespace NeoStackTextApp.Services;
+
+using System;
+
+using Models;
+
+public class CalculationService
+{
+    #region Methods
+
+    public void Calculate(InternalFunction function)
+    {
+        if (!function.AreCoefficientsSet())
+        {
+            return;
+        }
+
+        foreach (Arguments arguments in function.CoordinatesList)
+        {
+            if (arguments.X.HasValue && arguments.Y.HasValue)
+            {
+                arguments.Result = function.A * Math.Pow(arguments.X.Value, function.Degree) +
+                                   function.B * Math.Pow(arguments.Y.Value, function.Degree - 1) +
+                                   function.C;
+            }
+            else
+            {
+                arguments.Result = null;
+            }
+        }
+    }
+
+    #endregion
+}
