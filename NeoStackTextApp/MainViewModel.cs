9031e544efcdf815c8474ccab137f8c9f3d42401
SakharovDN <deafsah@gmail.com>
2022-09-19 23:03:21 +0700

    Added main view model

---
+++ NeoStackTextApp/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿namespace NeoStackTextApp;
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using Models;
+
+using Services;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    #region Fields
+
+    private readonly CalculationService _calculationService;
+    private InternalFunction _selectedFunction;
+
+    #endregion
+
+    #region Properties
+
+    public InternalFunction SelectedFunction
+    {
+        get => _selectedFunction;
+        set
+        {
+            _selectedFunction = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ObservableCollection<InternalFunction> Functions { get; }
+
+    #endregion
+
+    #region Events
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    #endregion
+
+    #region Constructors
+
+    public MainViewModel()
+    {
+        _calculationService = new CalculationService();
+        Functions = new ObservableCollection<InternalFunction>();
+
+        for (int i = 1; i < 6; i++)
+        {
+            Functions.Add(new InternalFunction((FunctionDegree)i));
+        }
+
+        _selectedFunction = Functions.First();
+    }
+
+    #endregion
+
+    #region Methods
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    #endregion
+}
--- NeoStackTextApp/Models/[Enums]/FunctionDegree.cs
+++ NeoStackTextApp/Models/[Enums]/FunctionDegree.cs
@@ -1,10 +1,21 @@
 ﻿namespace NeoStackTextApp.Models;
 
+using System.ComponentModel;
+
 public enum FunctionDegree
 {
+    [Description("Функция первой степени")]
     First = 1,
+
+    [Description("Функция второй степени")]
     Second = 2,
+
+    [Description("Функция третьей степени")]
     Third = 3,
+
+    [Description("Функция четвертой степени")]
     Forth = 4,
+
+    [Description("Функция пятой степени")]
     Fifth = 5,
 }
